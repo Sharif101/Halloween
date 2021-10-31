00d02a616271a885c46a6cfb0127dccaa8a0aabd
Sharif101 <sharif009009@gmail.com>
2021-10-31 19:02:39 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+gsap.to("#img", {
+    scrollTrigger : {
+        scrub:0.7,
+    },
+    x : 1500,
+    y : 500,
+})
+
+gsap.from("#wolf", {
+    scrollTrigger : {
+        scrub:0.7,
+    },
+    x : 300,
+})
+
+gsap.from("#castle", {
+    scrollTrigger : {
+        scrub:0.7,
+    },
+    x : 100,
+})
+
+gsap.from("#bats", {
+    scrollTrigger : {
+        scrub:0.5,
+    },
+    scale : 1.5,
+})
